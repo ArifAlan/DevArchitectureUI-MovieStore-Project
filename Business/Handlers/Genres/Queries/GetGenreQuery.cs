28fe868cd86c88defa4fde2e3b3bb376396a6c60
KaganDogan <kagandoganx@gmail.com>
2022-07-28 20:56:20 +0300

    Added query handler for actors directors genres movies

---
+++ Business/Handlers/Genres/Queries/GetGenreQuery.cs
@@ -0,0 +1,34 @@
+﻿using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Business.Handlers.Genres.Queries
+{
+    public class GetGenreQuery : IRequest<IDataResult<Genre>>
+    {
+        public int Id { get; set; }
+
+        class GetGenreQueryHandler : IRequestHandler<GetGenreQuery, IDataResult<Genre>>
+        {
+            private readonly IGenreRepository _genreRepository;
+
+            public GetGenreQueryHandler(IGenreRepository genreRepository)
+            {
+                _genreRepository = genreRepository;
+            }
+
+            public async Task<IDataResult<Genre>> Handle(GetGenreQuery request, CancellationToken cancellationToken)
+            {
+                var genre = await _genreRepository.GetAsync(p => p.Id == request.Id);
+
+                return new SuccessDataResult<Genre>(genre);
+            }
+        }
+    }
+}
